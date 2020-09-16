be61dc6e1c12cf85f949f0c115025c4a1de9eadc
Marcin Kowalicki <kowalickimarcin13@gmail.com>
2020-09-16 21:11:40 +0200

    Add files

---
+++ commands/random.js
@@ -0,0 +1,27 @@
+module.exports = {
+	name: 'random',
+	description: 'Send a random quote',
+	execute(message, args) {
+		const fs = require('fs');
+		const Discord = require('discord.js');
+
+		const rawdata1 = fs.readFileSync('quotes.json');
+		const rawdata = JSON.parse(rawdata1);
+		const number = Math.floor(Math.random() * 2);
+		var quote = rawdata[number].quote;
+		var episode = rawdata[number].episode;
+
+		const exampleEmbed = new Discord.MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('HRejterzy')
+			.setURL('https://www.codetwo.pl/praca/o-hr-wiemy-wszystko')
+			.addFields(
+				{ name: 'Cytat:', value:`${quote}` },
+				{ name: 'Odcinek:', value: `nic` },
+			)
+			.setTimestamp()
+			.setFooter('By MarcinK50#9775', 'https://github.com/MarcinK50');
+
+		message.channel.send(exampleEmbed);
+	},
+};
---
+++ main.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const { Client, MessageEmbed, Collection } = require('discord.js');
+const { prefix, token } = require('./config.json');
+
+const client = new Client();
+client.commands = new Collection();
+
+const commandFiles = fs
+	.readdirSync('./commands')
+	.filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+//  GitHub API user
+client.on('message', (message) => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'random') {
+  	client.commands.get('random').execute(message, args);
+	}
+});
+
+client.login(token);
